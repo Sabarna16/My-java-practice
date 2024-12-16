7c3d6d944d77887b6bfa2c447b706e7e706c150a
Sabarna16 <sabarnaj2006@gmail.com>
2024-12-16 15:39:03 +0530

    Create Agecalculation.c

---
+++ Agecalculation.c
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=a+3;
+        int Age=(4*b)-3;
+        System.out.println(Age);
+    }
+}
